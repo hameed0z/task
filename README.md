8d4d62eb756827a8262793d749d42bd57222a079
Abdulhameed Abdulmuttalib <abdulhameed.abdulmuttalib@gmail.com>
2024-07-16 12:18:27 +0300

    update script to use headless browser

--- README.md
+++ README.md
@@ -1,10 +1,31 @@
 ## Pre run
 
-the script uses request pkg, the easiest way to move forward is to install it using pip3.
+The script uses selenium headless browser as the fetched site relies on JS to load content, the easiest way to move forward is to install it using pip3.
 
 ```sh
-pip3 install requests
+pip3 install selenium 
+pip3 install webdriver_manager
 ```
+
+The script will run Chrome in headless mode, navigate to the URL, and print the source of the page.
+Note: If chrome is not already installed
+you will need to install it using the below commands 
+
+### Mac
+```sh
+brew install --cask google-chrome
+```
+
+### Linux
+
+```sh
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+```
+
+### Windows
+
+Unfortunately you will need to install it from chrome website
 
 ## Run
 
--- fetch_url.py
+++ fetch_url.py
@@ -1,9 +1,40 @@
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support.expected_conditions import url_changes
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.action_chains import ActionChains
 
 def get_request(url):
-    response = requests.get(url)
-    return response.text
+    chrome_options = Options()
+    chrome_options.add_argument("--headless") # Ensure GUI is off
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--enable-javascript")
+    chrome_options.add_argument("--enable-cookies")
 
-url = 'http://proscheduler.prometric.com/?prg=SCHS&path=seatavail'
-print(get_request(url))
+    webdriver_service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=webdriver_service, options=chrome_options)
+    
+    driver.get(url)
+    # Wait for the URL to change
+    WebDriverWait(driver, 10).until(url_changes(url))
+    # Wait for the loader to disappear, mean data is loaded
+    loader_class_name = "loader-wrpr"
+    WebDriverWait(driver, 10).until(EC.invisibility_of_element_located((By.CLASS_NAME, loader_class_name)))
 
+    page_source = driver.page_source
+    driver.quit()
+    
+    return page_source
+
+url = 'https://proscheduler.prometric.com/?prg=SCHS&path=seatavail'
+output = get_request(url)
+
+# We can use BeautifulSoup to fetch the data from the output html and make it more organized, but requirements doesn't say so
+# So, I will just write the output to a file
+with open('output.html', 'w') as f:
+    f.write(output)
